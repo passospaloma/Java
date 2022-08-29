52a1ecdd19d2937c89c1eebbe9d45e9fd6d0bd1c
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-08-28 18:27:13 -0700

    Update DisplayPyramid.java

--- DisplayPyramid.java
+++ DisplayPyramid.java
@@ -2,9 +2,7 @@
  *Program Name: DisplayPyramid.java
  *Author: Paloma Passos Vieira dos Santos 
  *Date: February 19, 2022
- *Course: CPSC 1150 – W01
- *Instructor: Leon Pan
-*/
+ */
 import java.util.Scanner;
 
 public class DisplayPyramid {
